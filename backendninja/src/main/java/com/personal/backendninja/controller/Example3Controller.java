631d25e27a98e61c6f7af7222b8b34378359f9f9
Miguel Angel <dippersoft@gmail.com>
2018-09-21 08:00:20 -0500

    Ejemplo de hibernate
    Se anexan archivos con ejemplo de hibernate

--- backendninja/src/main/java/com/personal/backendninja/controller/Example3Controller.java
+++ backendninja/src/main/java/com/personal/backendninja/controller/Example3Controller.java
@@ -1,7 +1,10 @@
 package com.personal.backendninja.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,8 +27,17 @@ public class Example3Controller {
 	}
 
 	@PostMapping("/addperson")
-	public ModelAndView addPerson(@ModelAttribute("person") Person person) {
-		ModelAndView mav = new ModelAndView(RESULT_VIEW);
+	public ModelAndView addPerson(@Valid @ModelAttribute("person") Person person, BindingResult bindingResult) {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		if(bindingResult.hasErrors()) {
+			mav.setViewName(FORM_VIEW);
+			
+		}else {
+			mav.setViewName(RESULT_VIEW);
+		}
+		
 		mav.addObject("person", person);
 		return mav;
 	}
